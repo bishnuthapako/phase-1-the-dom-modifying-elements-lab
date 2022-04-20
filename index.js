e28a6259ca8a681b8aea537634187fab99a0f825
Bishnu <bishnuthapako@gmail.com>
2022-04-20 12:40:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+document.body.appendChild(newHeader)
+newHeader.innerHTML = '\'\'Bishnu \'is the champion\'';
+
+// const newHeaderText = document.createTextNode('\'\'Bishnu \'is the champion\'')
+
+// newHeader.appendChild(newHeaderText);
+
